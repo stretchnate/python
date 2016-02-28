f091534ecb1496859ca24fa821103ff4a43e6710
dn8 <d.nate@tricorind.com>
2016-02-28 08:35:59 -0500

    fixed an issue with dealing two of the same card in one round

--- blackjack/dealer.py
+++ blackjack/dealer.py
@@ -6,10 +6,16 @@ deck = [
         ['Spades','Hearts','Clubs','Diamonds']
     ]
 
+dealt = []
+
 def deal(person):
     card = random.choice(deck[0]) + '[' + random.choice(deck[1])+']'
-    if card not in person:
-        person.append(card)
+
+    while card in dealt:
+        card = random.choice(deck[0]) + '[' + random.choice(deck[1])+']'
+        
+    dealt.append(card)
+    person.append(card)
 
     return person
 
